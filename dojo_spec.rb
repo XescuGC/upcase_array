fc455c47aaf34e05ba1c5fb03e15131fa40a04ba
xescuGC <xescugil@gmail.com>
2014-08-15 11:55:57 +0200

    Test for the base case, nil

--- dojo_spec.rb
+++ dojo_spec.rb
@@ -1,4 +1,13 @@
 require_relative 'dojo'
 
 describe Dojo do
+  let(:dojo) { Dojo.new }
+
+  describe "#return_inmutable_array" do
+    context "must return nil with nil param" do
+      it {
+        expect(dojo.return_inmutable_array(nil)).to be_nil
+      }
+    end
+  end
 end
