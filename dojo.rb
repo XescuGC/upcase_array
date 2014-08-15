9f618a0e47b4069711e5b830543c32ec0386071c
xescuGC <xescugil@gmail.com>
2014-08-15 13:07:14 +0200

    Implementation to return [] or [0] for one word

--- dojo.rb
+++ dojo.rb
@@ -1,6 +1,8 @@
 class Dojo
   def return_inmutable_array word
     return nil unless word
-    return []
+    return [] if word.empty?
+    return /[[:upper:]]/.match(word) ? [0] : []
+    else
   end
 end
