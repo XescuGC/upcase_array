17cc6a1c78836149b60f9581406cf98d8eec4b9e
xescuGC <xescugil@gmail.com>
2014-08-15 19:55:40 +0200

    Implementation for 2 and more words

--- dojo.rb
+++ dojo.rb
@@ -2,7 +2,15 @@ class Dojo
   def return_inmutable_array word
     return nil unless word
     return [] if word.empty?
-    return /[[:upper:]]/.match(word) ? [0] : []
+    return _array_of_upcase_positions word, 0
+  end
+
+  def _array_of_upcase_positions word, index, result=[]
+    return result if index > word.length
+    if /[[:upper:]]/.match(word[index])
+      return _array_of_upcase_positions(word, index + 1, (result + [index]))
     else
+      return _array_of_upcase_positions(word, index + 1, result)
+    end
   end
 end
